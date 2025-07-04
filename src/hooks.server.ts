fd1b51d572ef74e5b145a4f94b6e1d4269a4e1ce
Ryan Loader <ryan@triver.com>
2025-07-04 16:26:14 +0100

    Run 'npx @sentry/wizard@latest -i sveltekit'

---
+++ src/hooks.server.ts
@@ -0,0 +1,18 @@
+import { sequence } from '@sveltejs/kit/hooks';
+import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit';
+import * as Sentry from '@sentry/sveltekit';
+
+Sentry.init({
+	dsn: 'https://989d34ebfba07be45dd9251bb80e5b2a@o4505793464107008.ingest.us.sentry.io/4509610974707712',
+
+	tracesSampleRate: 1.0
+
+	// uncomment the line below to enable Spotlight (https://spotlightjs.com)
+	// spotlight: import.meta.env.DEV,
+});
+
+// If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
+export const handle = sequence(sentryHandle());
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
