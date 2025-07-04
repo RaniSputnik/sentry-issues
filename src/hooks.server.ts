438396f32ab8305c90fd953d468cb69b38efa07d
Ryan Loader <ryan@triver.com>
2025-07-04 16:48:40 +0100

    Follow https://docs.sentry.io/platforms/javascript/guides/cloudflare/frameworks/sveltekit/

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,18 +1,14 @@
 import { sequence } from '@sveltejs/kit/hooks';
-import { handleErrorWithSentry, sentryHandle } from '@sentry/sveltekit';
-import * as Sentry from '@sentry/sveltekit';
-
-Sentry.init({
-	dsn: 'https://989d34ebfba07be45dd9251bb80e5b2a@o4505793464107008.ingest.us.sentry.io/4509610974707712',
-
-	tracesSampleRate: 1.0
-
-	// uncomment the line below to enable Spotlight (https://spotlightjs.com)
-	// spotlight: import.meta.env.DEV,
-});
+import { handleErrorWithSentry, sentryHandle, initCloudflareSentryHandle } from '@sentry/sveltekit';
 
 // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
-export const handle = sequence(sentryHandle());
+export const handle = sequence(
+	initCloudflareSentryHandle({
+		dsn: 'https://b473eacf54d77917c8440f75683345ce@o4505793464107008.ingest.us.sentry.io/4509605102682112',
+		tracesSampleRate: 1.0,
+	}),
+	sentryHandle()
+);
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
 export const handleError = handleErrorWithSentry();
--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,6 @@ export default defineConfig({
 			}
 		}),
 		sveltekit()
-	]
+	],
+	optimizeDeps: { exclude: ["fsevents"] },
 });
