749bff6751814de30502f24a84b911a4f575772e
Ryan Loader <ryan@triver.com>
2025-07-08 17:57:06 +0100

    Redirect on server hook

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,14 @@
 import { sequence } from '@sveltejs/kit/hooks';
 import { handleErrorWithSentry, sentryHandle, initCloudflareSentryHandle } from '@sentry/sveltekit';
+import { redirect, type Handle } from '@sveltejs/kit';
+
+const redirectToSentryPage: Handle = async ({ event, resolve }): Promise<Response> => {
+	if (!event.request.url.includes("sentry")) {
+		return redirect(303, "sentry-example-page");
+	}
+	return resolve(event)
+}
+
 
 // If you have custom handlers, make sure to place them after `sentryHandle()` in the `sequence` function.
 export const handle = sequence(
@@ -7,7 +16,8 @@ export const handle = sequence(
 		dsn: 'https://989d34ebfba07be45dd9251bb80e5b2a@o4505793464107008.ingest.us.sentry.io/4509610974707712',
 		tracesSampleRate: 1.0,
 	}),
-	sentryHandle()
+	sentryHandle(),
+	redirectToSentryPage,
 );
 
 // If you have a custom error handler, pass it to `handleErrorWithSentry`
